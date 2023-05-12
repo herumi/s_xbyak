9474b0f3eb7e44b5eedb9cadcacbaab8bb561822
MITSUNARI Shigeo <herumi@nifty.com>
2023-05-12 11:01:25 +0900

    ignore blanck

--- test/win-test.bat
+++ test/win-test.bat
@@ -12,10 +12,10 @@ exit /b
 echo test %FILE%
 echo gas
 python3 %FILE%.py -m gas > a.txt
-diff %FILE%_gas.txt a.txt
+diff -w %FILE%_gas.txt a.txt
 echo nasm
 python3 %FILE%.py -win > b.txt
-diff %FILE%_win.txt b.txt
+diff -w %FILE%_win.txt b.txt
 echo masm
 python3 %FILE%.py -win -m masm > c.txt
-diff %FILE%_masm.txt c.txt
+diff -w %FILE%_masm.txt c.txt
