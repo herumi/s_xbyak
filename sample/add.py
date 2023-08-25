29dffa01aa18c8809b5a2d6bd3a2f30ecee1c794
MITSUNARI Shigeo <herumi@nifty.com>
2023-08-25 09:30:50 +0900

    [sample] add add_avx512

--- sample/add.py
+++ sample/add.py
@@ -15,6 +15,24 @@ def main():
       y = sf.p[1]
       lea(rax, ptr(x + y))
 
+  with FuncProc('add_avx512'):
+     with StackFrame(4, vNum=1, vType=T_ZMM) as sf:
+      pz = sf.p[0]
+      px = sf.p[1]
+      py = sf.p[2]
+      n = sf.p[3]
+      lpL = Label()
+
+      L(lpL)
+      vmovups(zmm0, ptr(px))
+      vaddps(zmm0, zmm0, ptr(py))
+      vmovups(ptr(pz), zmm0)
+      add(px, 64)
+      add(py, 64)
+      add(pz, 64)
+      sub(n, 16)
+      jnz(lpL)
+
   term()
 
 if __name__ == '__main__':
