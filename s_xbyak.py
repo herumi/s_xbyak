6ca31848f63c10b72ca15540a4830be4c0333a41
MITSUNARI Shigeo <herumi@nifty.com>
2025-03-05 17:54:28 +0900

    v0.9.9

--- s_xbyak.py
+++ s_xbyak.py
@@ -7,7 +7,7 @@ import struct
 import re
 import argparse
 
-VERSION="0.9.8"
+VERSION="0.9.9"
 
 def getDefaultParser(description='s_xbyak'):
   parser = argparse.ArgumentParser(description=description)
